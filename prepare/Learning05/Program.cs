aaebfc070a8748b74b6ba912216864cb4c55b901
Davy Berrett <ber23043@byui.edu>
2025-03-04 10:14:32 -0700

    Lesson 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shapes> shapes = new List<Shapes>();
+
+        Square s1 = new Square("Red", 3.4);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 4, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 17);
+        shapes.Add(s3);
+
+        foreach (Shapes s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Drawing;
+
+public class Square : Shapes
+{
+    private double _side;
+
+    public Square(string color, double side) : base (color)
+    {
+        _side = side;
+    }
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
